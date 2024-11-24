400376d298b427fb6ca90415d4a6ca3084d84a7f
Okkar25 <okkaaung2508@gmail.com>
2024-11-24 19:53:18 +0630

    Database for User and Bookmarks code setup

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,12 @@
-from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, Blueprint
 from flask_cors import CORS
 from waitress import serve
 from flask_migrate import Migrate
 from flask_restful import Resource, Api, marshal_with, fields
 import os
+from src.database import db
+from src.auth import auth
+from src.bookmarks import bookmarks
 
 
 def create_app(test_config=None):
@@ -13,23 +15,22 @@ def create_app(test_config=None):
 
     if test_config is None:
         app.config.from_mapping(
-            SECRET_KEY=os.environ.get("SECRET_KEY")
+            SECRET_KEY=os.environ.get("SECRET_KEY"),
+            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI"),
         )  # default configuration
     else:
         app.config.from_mapping(test_config)
 
-    @app.route("/")
-    def index():
-        return "<h2>Flask Bookmarks REST API</h2>"
+    db.app = app
+    db.init_app(app)
 
-    @app.route("/hello")
-    def hello():
-        return jsonify({"message": "Hello World"})
+    app.register_blueprint(auth)
+    app.register_blueprint(bookmarks)
 
     return app
 
 
-if __name__ == "__main__":
-    app = create_app()
-    # serve(app, host="0.0.0.0", port=5000)
-    app.run(host="0.0.0.0", port=8000, debug=True)
+# if __name__ == "__main__":
+#     app = create_app()
+#     # serve(app, host="0.0.0.0", port=5000)
+#     app.run(host="0.0.0.0", port=8000, debug=True)
---
+++ src/database.py
@@ -0,0 +1,58 @@
+from flask_sqlalchemy import SQLAlchemy
+from enum import unique
+from datetime import datetime
+import string
+import random
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.Text(), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())  # automatic
+    updated_at = db.Column(db.DateTime, onupdate=datetime.now())
+    bookmarks = db.relationship("Bookmark", backref="user", lazy=True)
+
+    def __repr__(self) -> str:
+        return f"User >>> {self.username}"
+
+
+class Bookmark(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text, nullable=True)
+    url = db.Column(db.Text, nullable=False)
+    short_url = db.Column(db.String(6), nullable=False)
+    visits = db.Column(db.Integer, default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, onupdate=datetime.now())
+
+    def generate_short_characters(self):
+        characters = string.digits + string.ascii_letters
+        SHORT_URL_LENGTH = 6
+        # picked_chars = "".join(random.choices(characters, k=SHORT_URL_LENGTH))
+
+        # Check if the short URL is unique
+        # checking if the generated short_url already exist in database
+        # link = self.query.filter_by(short_url=picked_chars).first()
+        # if link:
+        #     self.generate_short_characters()
+        # else:
+        #     return picked_chars
+
+        while True:
+            picked_chars = "".join(random.choices(characters, k=SHORT_URL_LENGTH))
+            if not self.query.filter_by(short_url=picked_chars).first():
+                return picked_chars
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        if not self.short_url:
+            self.short_url = self.generate_short_characters()
+
+    def __repr__(self) -> str:
+        return f"Bookmark >>> {self.url}"
